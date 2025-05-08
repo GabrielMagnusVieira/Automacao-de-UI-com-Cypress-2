ae8232368ff6ffa47dc15dc38503b20c8bea9065
justaus3r <magnus181098@gmail.com>
2025-05-08 18:44:04 -0300

    1

--- teste-e2e-ebac/cypress/e2e/exercicio-e2e.cy.js
+++ teste-e2e-ebac/cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
@@ -9,14 +11,60 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       Preenchendo todas opções no checkout
       E validando minha compra ao final */
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
-
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
       
+
+ 
   });
 
+  it.only('Deve fazer login com sucesso' , () => {
+//login
+        cy.visit('minha-conta/')
+        cy.get('#username').type('magnus181098@gmail.com')
+        cy.get('#password').type('dilolilo22.')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' ,'Olá')
+//produto 1
+        cy.visit('produtos/')
+        cy.get('.post-2559 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.single_add_to_cart_button').click()
+//produto 2
+        cy.visit('produtos/')
+        cy.get('.post-3111 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Brown').click()
+        cy.get('.single_add_to_cart_button').click()
+//produto3
+        cy.visit('produtos/')
+        cy.get('.post-3073 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+        cy.get('.button-variable-item-33').click()
+        cy.get('.button-variable-item-Brown').click()
+        cy.get('.single_add_to_cart_button').click()
+//produto 4
+        cy.visit('produtos/')
+        cy.get('.post-2622 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.single_add_to_cart_button').click()
+//carrinho            
+        cy.visit('carrinho/')
+        cy.get('.checkout-button').click()
+        cy.get('#billing_first_name').type('Gabriel')
+        cy.get('#billing_last_name').type('Magnus')
+        cy.get('#billing_address_1').type('Padre Joao Batista Reus 1959')
+        cy.get('#billing_city').type('Porto Alegre')
+        cy.get('#select2-billing_state-container')
+        cy.get('#billing_postcode').type('91920000')
+        cy.get('#billing_phone').type('51994041075')
+        cy.get('#payment_method_cod').click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+        
+    });
 
-})
+
+
+
+
+        
